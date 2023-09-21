a4c98b94b1487696c131866351c0d9725ff76f7f
Billy Zovan Ginting <billyzovanginting@gmail.com>
2023-09-21 17:58:30 +0700

    portofolio is done

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const menuToggle = document.querySelector(".toggle");
+const navItem = document.querySelector("nav ul");
+menuToggle.addEventListener("click", () => {
+  navItem.classList.toggle("slide");
+});
+
+const boxes = document.querySelectorAll(".about,.experience,.skill,.education,interest,.awards");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const trigger = (window.innerHeight / 5) * 4;
+
+  for (let i = 0; i < boxes.length; i++) {
+    const boxTop = boxes[i].getBoundingClientRect().top;
+    if (boxTop < trigger) {
+      boxes[i].classList.add("show");
+    } else {
+      boxes[i].classList.remove("show");
+    }
+  }
+}
